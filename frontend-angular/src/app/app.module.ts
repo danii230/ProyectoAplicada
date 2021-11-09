cd4b666cd14fde04e050615c4cfeffa6b3aee1aa
valerialeiva <44216036+valerialeiva@users.noreply.github.com>
2021-11-09 10:43:48 -0600

    Revert "Merge branch 'main' of https://github.com/danii230/ProyectoAplicada into Daniela"
    This reverts commit da3166a8e7fd61209e27aaab15fa478602c3d1c6, reversing
    changes made to 76a0610a4cac3d74689c68928b19fb8f93a8bc57.

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -14,13 +14,6 @@ import { MainComponent } from './main/main.component';
 
 //Angular Material
 import {MatCardModule} from '@angular/material/card';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-
 
 const appRoutes: Routes = [
   {
@@ -37,8 +30,7 @@ const appRoutes: Routes = [
     path: '',
     redirectTo: '/login',
     pathMatch: 'full'
-  },
-  
+  }
 
 ];
 
@@ -56,18 +48,7 @@ const appRoutes: Routes = [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    MatCardModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatDividerModule,
-    MatButtonModule,
-    MatMenuModule
-  ],
-  exports: [
-    MatSidenavModule,
-    MatToolbarModule,
-    MatMenuModule
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend-angular/src/app/main/main.component.ts
+++ frontend-angular/src/app/main/main.component.ts
@@ -1,37 +1,15 @@
-import { Component, OnInit,ViewChild  } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { RestService } from '../rest.service';
-import { BreakpointObserver } from '@angular/cdk/layout';
-import { MatSidenav } from '@angular/material/sidenav';
-import { delay } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 
-  @ViewChild(MatSidenav)
-  sidenav!: MatSidenav;
+  constructor() { }
 
-  constructor(private observer: BreakpointObserver) {}
-
-
-
-  ngAfterViewInit() {
-    this.observer
-      .observe(['(max-width: 800px)'])
-      .pipe(delay(1))
-      .subscribe((res) => {
-        if (res.matches) {
-          this.sidenav.mode = 'over';
-          this.sidenav.close();
-        } else {
-          this.sidenav.mode = 'side';
-          this.sidenav.open();
-        }
-      });
+  ngOnInit(): void {
   }
+
 }
