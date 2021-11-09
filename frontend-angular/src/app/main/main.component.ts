402b9a9b1fd38f14919dadd9bed0250843b36dda
valerialeiva <44216036+valerialeiva@users.noreply.github.com>
2021-11-09 10:44:47 -0600

    Revert "Revert "Merge branch 'main' of https://github.com/danii230/ProyectoAplicada into Daniela""
    This reverts commit cd4b666cd14fde04e050615c4cfeffa6b3aee1aa.

--- frontend-angular/src/app/main/main.component.ts
+++ frontend-angular/src/app/main/main.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild  } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RestService } from '../rest.service';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent {
 
-  constructor() { }
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
 
-  ngOnInit(): void {
+  constructor(private observer: BreakpointObserver) {}
+
+
+
+  ngAfterViewInit() {
+    this.observer
+      .observe(['(max-width: 800px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
   }
-
 }
